9e6daaec0a9afebb67cfb326cdb42d3e2ce3928b
p0tkun <itzmaddylolz23@gmail.com>
2021-12-12 08:18:59 +0000

    vercel deploy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -49,7 +49,6 @@
 </section>
 <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
-    <button class="btn btn-secondary"  on:click={signOut} 
-    >Logout</button>
+    <button class="btn btn-secondary"  on:click={signOut}>Logout</button>
 </section>
 <EntryModal />
